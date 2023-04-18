6d955d6c69ac28d9f70c16df83055f2bf9e168c7
markiewee <markwee99@gmail.com>
2023-04-18 16:53:03 +0800

    finally done pending checks

--- linreg.py
+++ linreg.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from datetime import timedelta
 from sklearn.linear_model import LinearRegression
 import stock_strategy
+import pandas_market_calendars as mcal
 
 class Linreg:
 
@@ -40,41 +41,77 @@ class Linreg:
 
         return merged_df
     
-    def predict_performance(self):
-        merged_df = self.merge_data()
-
-        X = merged_df[['Return_strategy_1', 'Return_strategy_2']]
-        y = merged_df['Return_actual']
-
+    def predict_performance(self, X_train, y_train, X_test):
         # Fit a linear regression model
         model = LinearRegression()
-        model.fit(X, y)
+        model.fit(X_train, y_train)
 
         # Predict stock returns
-        predicted_returns = model.predict(X)
-        merged_df['Predicted_Return'] = predicted_returns
+        predicted_returns = model.predict(X_test)
+        return predicted_returns
+    
 
-        return merged_df
 
     def perform_strategy(self):
         """Fits a multiple linear regression model to predict stock returns and selects the top stocks based on the --top_pct score."""
+        nyse = mcal.get_calendar('NYSE')
+        schedule = nyse.schedule(start_date=self.start_date, end_date=self.end_date)
+        last_trading_days = schedule.resample('M', closed='right', label='right').last().index
 
-        # Predict stock returns using the linear regression model
-        predicted_data_df = self.predict_performance()
+        aum = self.aum
 
-        # Select the top stocks based on the predicted returns
-        top_stocks = self.Strategy.compute_top_stocks(self.top_pct)
-        print(predicted_data_df)
+        for i in range(len(last_trading_days) - 1):
+            current_month_start = last_trading_days[i].strftime('%Y%m%d')
+            current_month_end = last_trading_days[i + 1].strftime('%Y%m%d')
+            prev_month_start = (last_trading_days[i] - pd.DateOffset(months=1)).strftime('%Y%m%d')
 
-        sorted_df = predicted_data_df.sort_values(by='Predicted_Return', ascending=False)
+
+            # Get data for previous and current months
+            prev_month_data = self.Strategy.get_data_for_all_tickers(prev_month_start, current_month_start, self.tickers)
+            current_month_data = self.Strategy.get_data_for_all_tickers(current_month_start, current_month_end, self.tickers)
+
+            # Run strategies for previous and current months
+            prev_month_strat1 = self.Strategy.run_strategy(prev_month_start, current_month_start, self.days_1, self.strategy_1, self.tickers)
+            prev_month_strat2 = self.Strategy.run_strategy(prev_month_start, current_month_start, self.days_2, self.strategy_2, self.tickers)
+            current_month_strat1 = self.Strategy.run_strategy(current_month_start, current_month_end, self.days_1, self.strategy_1, self.tickers)
+            current_month_strat2 = self.Strategy.run_strategy(current_month_start, current_month_end, self.days_2, self.strategy_2, self.tickers)
+
+            # Merge data
+            prev_month_merged = pd.merge(prev_month_strat1, prev_month_strat2, on='Stock', how='inner')
+            prev_month_actual_performance = self.Strategy.actual_performance(prev_month_start, current_month_start)
+            prev_month_merged = pd.merge(prev_month_merged, prev_month_actual_performance, on='Stock', how='inner')
+            prev_month_merged = prev_month_merged.rename(columns={'Return_x': 'Return_strategy_1', 'Return_y': 'Return_strategy_2', 'Return': 'Return_actual'})
+
+            current_month_merged = pd.merge(current_month_strat1, current_month_strat2, on='Stock', how='inner')
+            current_month_actual_performance = self.Strategy.actual_performance(current_month_start, current_month_end)
+            current_month_merged = pd.merge(current_month_merged, current_month_actual_performance, on='Stock', how='inner')
+            current_month_merged = current_month_merged.rename(columns={'Return_x': 'Return_strategy_1', 'Return_y': 'Return_strategy_2', 'Return': 'Return_actual'})
+
+            # Prepare the data for the regression model
+            X_train = prev_month_merged[['Return_strategy_1', 'Return_strategy_2']]
+            y_train = prev_month_merged['Return_actual']
+            X_test = current_month_merged[['Return_strategy_1', 'Return_strategy_2']]
+
+            # Predict stock returns using the linear regression model
+            predicted_returns = self.predict_performance(X_train, y_train, X_test)
+            current_month_merged['Predicted_Return'] = predicted_returns
+
+            top_stocks = self.Strategy.compute_top_stocks(self.top_pct)
+            sorted_df = current_month_merged.sort_values(by='Predicted_Return', ascending=False)
+            top_pct_df = sorted_df.head(top_stocks)
+            # Store the top-performing stocks for each month
+            if i == 0:
+                all_top_stocks = top_pct_df
+            else:
+                all_top_stocks = all_top_stocks.append(top_pct_df, ignore_index=True)
+            # Calculate average return and update AUM
+            average_return = top_pct_df['Return_actual'].mean()
+            aum = (aum * (1 + average_return))
+
+        return aum, all_top_stocks
+
     
-        top_pct_df = sorted_df.head(top_stocks)
     
-        average_return = top_pct_df['Return_actual'].mean()
-        print(average_return)
-        updated_aum = (self.aum * average_return)+self.aum
-    
-        return updated_aum
 
         
 
@@ -94,15 +131,17 @@ aum = 100000
 # Create an instance of the Linreg class
 linreg = Linreg(tickers, start_date, end_date, days_1, day_2, strategy_1, strategy_2, top_pct,aum)
 
-# Perform the strategy and get the top stocks
-top_stocks = linreg.perform_strategy()
-print(top_stocks)
+
+updated_aum, top_stocks = linreg.perform_strategy()
+print(updated_aum)
 
 # Plot the graph comparing the actual and predicted returns of the top stocks
 fig, ax = plt.subplots()
-x_labels = top_stocks['Stock'].tolist()
-ax.bar(x_labels, top_stocks['Return'], label='Actual', alpha=0.5, color='b')
-ax.bar(x_labels, top_stocks['Predicted_Return'], label='Predicted', alpha=0.5, color='r')
+x_labels = top_stocks['Stock'].unique().tolist()
+actual_returns = top_stocks.groupby('Stock')['Return_actual'].mean()
+predicted_returns = top_stocks.groupby('Stock')['Predicted_Return'].mean()
+ax.bar(x_labels, actual_returns, label='Actual', alpha=0.5, color='b')
+ax.bar(x_labels, predicted_returns, label='Predicted', alpha=0.5, color='r')
 ax.set_xlabel('Stocks')
 ax.set_ylabel('Returns (%)')
 ax.set_title('Comparison of Actual and Predicted Returns')
---
+++ portfolio_statistics.py
@@ -0,0 +1,84 @@
+import numpy as np
+import scipy.stats as stats
+from linreg import Linreg
+import pandas as pd
+
+
+class PortfolioStatistics:
+
+    def __init__(self, linreg_instance):
+        self.linreg = linreg_instance
+        self.top_stocks = linreg_instance.perform_strategy()[1]
+        self.daily_returns = self.top_stocks.groupby(pd.to_datetime(self.top_stocks['Date'])).mean()['Return_actual']
+        self.start_date = pd.to_datetime(self.linreg.start_date)
+        self.end_date = pd.to_datetime(self.linreg.end_date)
+        self.aum = self.linreg.aum
+
+    def adjust_dates(self):
+        nyse = mcal.get_calendar('NYSE')
+        schedule = nyse.schedule(start_date=self.start_date, end_date=self.end_date)
+        self.start_date = schedule.index[0]
+        self.end_date = schedule.index[-1]
+
+    def calculate_statistics(self):
+        self.adjust_dates()
+        num_days = (self.end_date - self.start_date).days
+        total_return = self.top_stocks['Return_actual'].sum()
+        initial_aum = self.aum
+        final_aum = self.linreg.perform_strategy()[0]
+        total_return_aum = final_aum - initial_aum
+        annualized_rate_of_return = (final_aum / initial_aum) ** (365.0 / num_days) - 1
+        avg_daily_aum = (self.daily_returns * initial_aum).mean()
+        max_daily_aum = (self.daily_returns * initial_aum).max()
+        pnl = total_return_aum
+        avg_daily_return = self.daily_returns.mean()
+        daily_std = self.daily_returns.std()
+        daily_sharpe_ratio = (avg_daily_return - 0.0001) / daily_std
+
+        # Linear regression coefficients and t-values
+        model = self.linreg.predict_performance()
+        coeffs = model.params
+        t_values = model.tvalues
+
+        statistics = {
+            'Begin date': self.start_date,
+            'End date': self.end_date,
+            'Number of days': num_days,
+            'Total stock return': total_return,
+            'Total return (AUM)': total_return_aum,
+            'Annualized rate of return (AUM)': annualized_rate_of_return,
+            'Initial AUM': initial_aum,
+            'Final AUM': final_aum,
+            'Average daily AUM': avg_daily_aum,
+            'Maximum daily AUM': max_daily_aum,
+            'PnL (AUM)': pnl,
+            'Average daily return of the portfolio': avg_daily_return,
+            'Daily Standard deviation of the return of the portfolio': daily_std,
+            'Daily Sharpe Ratio of the portfolio': daily_sharpe_ratio,
+            'Coefficients of the Linear regression': coeffs,
+            't-values of the Linear regression': t_values
+        }
+
+        return statistics
+
+if __name__ == '__main__':
+    tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA']
+    start_date = '20210105'
+    end_date = '20211228'
+    days_1 = 30
+    day_2 = 60
+    strategy_1 = 'M'
+    strategy_2 = 'R'
+    top_pct = 30
+    aum = 100000
+
+    linreg = Linreg(tickers, start_date, end_date, days_1, day_2, strategy_1, strategy_2, top_pct, aum)
+
+    linreg.perform_strategy()
+
+    portfolio_stats = PortfolioStatistics(linreg)
+
+    stats = portfolio_stats.calculate_statistics()
+
+    for key, value in stats.items():
+        print(f"{key}: {value}")
